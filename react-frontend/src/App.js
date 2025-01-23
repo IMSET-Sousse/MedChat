ddcd72fed5c0098ca879b5e4daff4574477b4277
ihebbenhassine <ihebbenhassine82@gmail.com>
2025-01-23 09:23:03 +0100

    components

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; 
+import Messages from './components/Messages';
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
